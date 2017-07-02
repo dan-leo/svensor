325f0d785f374a824e728d7a71a46ec56fbee8cc
Daniel Robinson <d7robin@gmail.com>
2017-07-02 21:51:32 +0200

    added MPU6050 lib

---
+++ .sloeber/main/.ino.cpp
@@ -0,0 +1,17 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2017-07-01 01:35:25
+
+#include "Arduino.h"
+#include "Arduino.h"
+#include <Wire.h>
+#include <MPU6050.h>
+void setup() ;
+void loop() ;
+
+#include "main.ino"
+
+
+#endif
